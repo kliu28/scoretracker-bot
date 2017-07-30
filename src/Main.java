153caaa8363fc2a3e2a690d78746e7ce419268f8
Katherine Liu <katherineliu28@gmail.com>
2017-07-30 13:48:05 -0700

    finished initial trials

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import java.io.FileInputStream;
 import java.util.Iterator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by Katherine Liu on 7/16/2017.
@@ -12,20 +17,30 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Check file path syntax.");
-            return;
-        }
+        //reading excel file
+//        if (args.length != 1) {
+//            System.out.println("Check file path syntax.");
+//            return;
+//        }
+//
+//        try {
+//            FileInputStream excelFile = new FileInputStream(args[0]);
+//            XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
+//            readWriteFile(workbook);
+//        }
+//        catch (Exception e) {
+//            System.out.println("Invalid file path.");
+//            return;
+//        }
 
-        try {
-            FileInputStream excelFile = new FileInputStream(args[0]);
-            XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
-            readWriteFile(workbook);
-        }
-        catch (Exception e) {
-            System.out.println("Invalid file path.");
-            return;
-        }
+        System.setProperty("webdriver.chrome.driver", "C:\\Projects\\scoretracker-bot\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://onlinescoretracker.com/user-panel/students");
+
+
+//        WebElement q = driver.findElement(By.name("q"));
+//        q.sendKeys("hello world");
+//        q.submit();
     }
 
     private static void readWriteFile(XSSFWorkbook workbook) {
